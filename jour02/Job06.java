b7f27a3052669228bf521b336aa96536207c1882
Salvatore A <salvatore.anania@laplateforme.io>
2023-05-10 10:51:52 +0200

    Ajout du Job06 jour02

---
+++ jour02/Job06.java
@@ -0,0 +1,9 @@
+public class Job06 {
+    public static void main(String[] args) {
+        int somme=0;
+        for (int i=0; i<=100;i+=2) {
+            somme+=i;
+            }
+        System.out.println(somme);
+    }
+}
