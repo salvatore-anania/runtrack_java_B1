949ba7f8d8bd121d106350aa44434a70fa5d290e
salvatore-anania <salvatore.anania@laplateforme.io>
2023-05-11 10:17:45 +0200

    Ajout du Job04 du jour03

---
+++ jour03/Job04.java
@@ -0,0 +1,25 @@
+public class Job04 {
+    public static void main(String[] args) {
+        int[] tableau = {3, 7, 3, 9, 8};
+        int [] frequency = new int [tableau.length];
+        int counted = -1;
+        for(int i = 0; i < tableau.length; i++){
+            int count = 1;
+            for(int j = i+1; j < tableau.length; j++){
+                if(tableau[i] == tableau[j]){
+                count++;
+        
+                frequency[j] = counted;
+                }
+            }
+            if(frequency[i] != counted)
+                frequency[i] = count;
+        }
+
+        for(int i = 0; i < frequency.length; i++){
+            if(frequency[i] != counted)
+                System.out.println("Element : "+tableau[i] + " occurence : " + frequency[i]);
+        }
+                
+        }
+}
