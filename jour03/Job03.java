aae89b9093bf7cee755920d87af6053114eef34b
salvatore-anania <salvatore.anania@laplateforme.io>
2023-05-11 10:11:54 +0200

    Ajout du jour03 du jour03

---
+++ jour03/Job03.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class Job03 {
+    public static void main(String[] args) {
+        String[] tableau = {"Josette", "John", "Myrtille","Marc"};
+        System.out.println(tableau[1]);
+        tableau[2]="Mireille";
+        System.out.println(Arrays.toString(tableau));
+            
+    }
+}
