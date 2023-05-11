9ccd455b4a1ce7ba08ab3411dc2fbf891e7c0337
salvatore-anania <salvatore.anania@laplateforme.io>
2023-05-11 12:43:31 +0200

    Ajout du Job08 du jour03

---
+++ jour03/Job08.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Job08 {
+    public static void main(String[] args) {
+        int[][] tableau = {{5,9,3},{7,2,8},{1,6,4}};
+        int i, j, temp;
+        int n=tableau.length;
+        int m=tableau[0].length;
+        for (i = 0; i < n * m - 1; ++i) {
+        for (j = 0; j < n * m - 1 - i; ++j) {
+        if (tableau[j / m][j % m] > tableau[(j + 1) / m][(j + 1) % m]) {
+        temp = tableau[(j + 1) / m][(j + 1) % m];
+        tableau[(j + 1) / m][(j + 1) % m] = tableau[j / m][j % m];
+        tableau[j / m][j % m] = temp;
+        System.out.println(Arrays.deepToString(tableau));
+        }
+        }
+        }
+        System.out.println(Arrays.deepToString(tableau));
+    }
+}
